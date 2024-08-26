386ec41a735f5b1770a6479855e28ca6b713332f
parknew0 <psy_chopath@naver.com>
2024-08-26 17:28:06 +0900

    8/26

--- TYPESCRIPTSTUDY/index825.js
+++ TYPESCRIPTSTUDY/index825.js
@@ -32,3 +32,80 @@ var 링크 = document.querySelectorAll(".naver");
         a.href = "https://kakao.com";
     }
 });
+var Person1 = /** @class */ (function () {
+    function Person1(a) {
+        this.data = 0;
+        this.name = a;
+    }
+    Person1.prototype.myfunc = function (a) {
+        console.log("안녕" + a);
+    };
+    return Person1;
+}());
+// Person1.prototype.myfunc = () => {
+// }
+var man1 = new Person1("kim");
+var man2 = new Person1("park");
+console.log(man1.data);
+man1.myfunc("ㅋㅋ");
+var Car = /** @class */ (function () {
+    function Car(model, price) {
+        this.model = model;
+        this.price = price;
+    }
+    Car.prototype.tax = function () {
+        console.log(this.price / 10);
+    };
+    return Car;
+}());
+var car1 = new Car("소나타", 3000);
+car1.tax();
+var Word = /** @class */ (function () {
+    function Word() {
+        var param = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            param[_i] = arguments[_i];
+        }
+        var numbers = [];
+        var words = [];
+        param.forEach(function (i) {
+            if (typeof i === "string") {
+                words.push(i);
+            }
+            else {
+                numbers.push(i);
+            }
+        });
+        this.num = numbers;
+        this.str = words;
+    }
+    return Word;
+}());
+var obj1 = new Word("kim", 3, 5, "park");
+console.log(obj1.num); //[3,5]
+console.log(obj1.str); //['kim', 'park']
+var square = { color: "red", width: 100 };
+var student = { name: "kim," };
+var teacher = { name: "kim", age: 20 };
+var product = {
+    brand: "Samsung",
+    serialNumber: 1360,
+    model: ["TV", "phone"],
+};
+var cart = [
+    { product: "청소기", price: 7000 },
+    { product: "삼다수", price: 800 },
+];
+var extendedcart = {
+    product: "청소기",
+    price: 7000,
+    card: false,
+};
+var myobj1 = {
+    plus: function (a, b) {
+        return a + b;
+    },
+    minus: function (a, b) {
+        return a - b;
+    },
+};
--- TYPESCRIPTSTUDY/index825.ts
+++ TYPESCRIPTSTUDY/index825.ts
@@ -40,3 +40,130 @@ let 링크 = document.querySelectorAll(".naver");
     a.href = "https://kakao.com";
   }
 });
+
+class Person1 {
+  data: number = 0;
+  name: string;
+  constructor(a: string) {
+    this.name = a;
+  }
+  myfunc(a: string) {
+    console.log("안녕" + a);
+  }
+}
+
+// Person1.prototype.myfunc = () => {
+
+// }
+
+let man1 = new Person1("kim");
+let man2 = new Person1("park");
+
+console.log(man1.data);
+man1.myfunc("ㅋㅋ");
+
+class Car {
+  model: string;
+  price: number;
+
+  constructor(model: string, price: number) {
+    this.model = model;
+    this.price = price;
+  }
+
+  tax(): void {
+    console.log(this.price / 10);
+  }
+}
+
+let car1 = new Car("소나타", 3000);
+car1.tax();
+
+class Word {
+  num;
+  str;
+
+  constructor(...param: (number | string)[]) {
+    let numbers: number[] = [];
+    let words: string[] = [];
+
+    param.forEach((i) => {
+      if (typeof i === "string") {
+        words.push(i);
+      } else {
+        numbers.push(i);
+      }
+    });
+
+    this.num = numbers;
+    this.str = words;
+  }
+}
+
+let obj1 = new Word("kim", 3, 5, "park");
+console.log(obj1.num); //[3,5]
+console.log(obj1.str); //['kim', 'park']
+
+interface Square {
+  color: string;
+  width: number;
+}
+
+let square: Square = { color: "red", width: 100 };
+
+interface Student {
+  name: string;
+}
+
+interface Teacher extends Student {
+  age: number;
+}
+
+let student: Student = { name: "kim," };
+let teacher: Teacher = { name: "kim", age: 20 };
+
+interface Product {
+  brand: string;
+  serialNumber: number;
+  model: string[];
+}
+
+let product: Product = {
+  brand: "Samsung",
+  serialNumber: 1360,
+  model: ["TV", "phone"],
+};
+
+interface Cart {
+  product: string;
+  price: number;
+}
+
+let cart: Cart[] = [
+  { product: "청소기", price: 7000 },
+  { product: "삼다수", price: 800 },
+];
+
+interface ExtendedCart extends Cart {
+  card: boolean;
+}
+
+let extendedcart: ExtendedCart = {
+  product: "청소기",
+  price: 7000,
+  card: false,
+};
+
+interface Mathz {
+  plus: (a: number, b: number) => number,
+  minus: (a: number, b: number) => number,
+}
+
+let myobj1 : Mathz = {
+  plus(a, b) {
+    return a+b
+  },
+  minus(a, b) {
+    return a-b
+  },
+}
